6042a255a176bdb5e62d33a94b62b49a83570bd5
Howie Cheng <howiechen@sjtu.edu.cn>
2019-07-22 21:23:54 +0800

    7/22

---
+++ leet_code/leet_code/112.path-sum.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=112 lang=python3
+#
+# [112] Path Sum
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        
+
